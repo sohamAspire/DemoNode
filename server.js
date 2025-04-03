df1eaa222adbb8cb97b8a628f3d4fb9dc723114f
sohamAspire <soham.patel@aspiresoftserv.com>
2025-04-03 14:53:40 +0530

    Trigger 2

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/test', (req, res) => {
-    res.status(200).send('Welcome onboard Owner!');
+    res.status(200).send('Welcome onboard!');
 });
 
 app.get('/cookies', (req, res) => {
